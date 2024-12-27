cf41074a8d737573dab8383639d930d571ffb8e3
gumer81 <144414018+gumer81@users.noreply.github.com>
2024-12-27 01:05:20 +0100

    Update jva.js

--- jva.js
+++ jva.js
@@ -1,1 +1,30 @@
+// Recargar la lista de archivos
+function jva1() {
+    var var1 = document.getElementById('input1').value;
+    var var2 = document.getElementById('input2').value;
+    var var3 = document.getElementById('input3').value;
+    // Llamada AJAX para obtener los archivos
+    jva2(var1, var2, var3);
+}
 
+// Función AJAX para obtener archivos
+function jva2(var4, var5, var6) {
+    // Implementación de llamada AJAX
+}
+
+// Girar imagen a la derecha
+function jva4() {
+    var var7 = document.getElementById('imageView').src;
+    jva3(var7, 'derecha');
+}
+
+// Girar imagen a la izquierda
+function jva5() {
+    var var8 = document.getElementById('imageView').src;
+    jva3(var8, 'izquierda');
+}
+
+// Función para llamar a ajx.php para rotar imagen
+function jva3(var9, var10) {
+    // Implementación de llamada AJAX para rotar imagen
+}
